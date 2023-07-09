91f59f570150cfb7579991e9a158a3ee031755fe
Kirs <kirsysuv@gmail.com>
2023-07-09 17:17:16 +0800

    thanks

---
+++ Assets/Thanks.cs
@@ -0,0 +1,22 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thanks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DOTween.Sequence().AppendInterval(2f).OnComplete(() =>
+        {
+            SceneTransition.Instance.StartFadeIn("InitScene");
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
